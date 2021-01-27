5c6a875ea58a5a90551baa38b3cf220528062e09
ivan montoya sanchez <montoyaivan97@gmail.com>
2021-01-27 12:07:52 +0100

    prototipo de vista de tematicas, sigue habiendo error con los seeders

--- resources/views/backend/theme/form.blade.php
+++ resources/views/backend/theme/form.blade.php
@@ -14,8 +14,8 @@
         @csrf
         Nombre:<input type="text" name="name" value="{{$theme->name ?? '' }}"><br>
         Imagen de la tematica:<input type="text" name="img" value="{{$theme->theme_img ?? '' }}"><br>
-        Posicion "X" de la tematica:<<input type="text" name="position_X" value="{{$theme->position_X ?? '' }}"></br>
-        Posicion "Y" de la tematica:<<input type="text" name="position_Y" value="{{$theme->position_Y ?? '' }}"></br>
+        Posicion "X" de la tematica:<<input type="number" name="position_X" value="{{$theme->position_X ?? '' }}"></br>
+        Posicion "Y" de la tematica:<<input type="number" name="position_Y" value="{{$theme->position_Y ?? '' }}"></br>
         <input type="submit">
         </form>
 
